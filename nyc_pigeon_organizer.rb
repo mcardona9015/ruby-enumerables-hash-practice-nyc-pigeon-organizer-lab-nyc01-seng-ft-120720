3d2ea14e1e23c22e46644737c189fef0f7b53ac4
Michael Cardona <mcardona9015@gmail.com>
2020-11-29 00:29:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}
+  data.each do |attribute, attribute_hash|
+    attribute_hash.each do |attribute_value, names|
+      names.each do |name|
+        if !organized_data[name]
+          organized_data[name] = {}
+        end
+        if !organized_data[name][attribute]
+          organized_data[name][attribute] = []
+        end
+        organized_data[name][attribute] << attribute_value.to_s
+      end
+    end
+  end
+  organized_data
 end
